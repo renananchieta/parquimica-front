8bd4827b895afaa78c33a162e02256c673a0420a
renananchieta <renangap18@gmail.com>
2024-06-24 20:41:51 -0300

    exportar csv

--- src/components/Catalogo/CatalogoInicial.vue
+++ src/components/Catalogo/CatalogoInicial.vue
@@ -99,12 +99,12 @@ const limparFiltros = () => {
 
 const exportarCSV = () => {
     carregando.value = true;
-    api.get('/catalogo/grid/exportar-csv', { responseType: 'blob' })
+    api.get('/catalogo/grid/exportar-csv', { responseType: 'blob' }) 
     .then((response) => {
-        const url = window.URL.createObjectURL(new Blob([response.data], { type: 'text/csv' }));
+        const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement('a');
         link.href = url;
-        link.setAttribute('download', response.headers['content-disposition'].split('filename=')[1].replace(/"/g, ''));
+        link.setAttribute('download', 'produtos.csv');
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
