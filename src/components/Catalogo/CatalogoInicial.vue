85f06829adab9bc17f3cdebace15db5087406ad1
renananchieta <renangap18@gmail.com>
2024-06-24 21:56:28 -0300

    Exportar CSV

--- src/components/Catalogo/CatalogoInicial.vue
+++ src/components/Catalogo/CatalogoInicial.vue
@@ -40,7 +40,7 @@
                 class="mt-n14 mr-4"
                 color="botao"
                 :loading="carregando"
-                @click="exportarCSV()">Exportar CSV</v-btn>
+                @click="exportData('csv')">Exportar CSV</v-btn>
             </v-card-actions>
             <v-card-text>
                 <v-data-table
@@ -97,25 +97,74 @@ const limparFiltros = () => {
 //    pesquisar();
 };
 
-const exportarCSV = () => {
-    carregando.value = true;
-    api.get('/catalogo/grid/exportar-csv', { responseType: 'blob' })
-    .then((response) => {
-        const url = window.URL.createObjectURL(new Blob([response.data]));
-        const link = document.createElement('a');
-        link.href = url;
-        link.setAttribute('download', 'produtos.csv');
-        document.body.appendChild(link);
-        link.click();
-        link.parentNode.removeChild(link);
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-    .finally(() => {
-        carregando.value = false;
-    });
+function jsonToCsv(json) {
+  if (json.length === 0) return '';
+  const keys = Object.keys(json[0]);
+  const csvRows = [];
+  csvRows.push(keys.join(','));
+  json.forEach(item => {
+    const values = keys.map(key => `"${item[key]}"`);
+    csvRows.push(values.join(','));
+  });
+  return csvRows.join('\n');
 }
+
+function exportData(format) {
+  const filteredData = dados.value; // Assume que filteredProducts.value contém os dados filtrados dos produtos
+
+  if (format === 'csv') {
+    const productsCsv = jsonToCsv(filteredData);
+    const bom = '\uFEFF';  // Adiciona o BOM para garantir a codificação UTF-8
+    const blob = new Blob([bom + productsCsv], { type: 'text/csv;charset=utf-8;' });
+    const link = document.createElement('a');
+    link.href = URL.createObjectURL(blob);
+    link.download = 'products_data.csv';
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+  } else if (format === 'pdf') {
+    const doc = new jsPDF();
+    doc.autoTable({
+      styles: { overflow: 'linebreak' },
+      bodyStyles: { valign: 'middle', fontSize: 10 },
+      columnStyles: {
+        0: { cellWidth: 'auto' },
+        1: { cellWidth: 'auto' },
+        2: { cellWidth: 'auto' },
+        3: { cellWidth: 'auto' }
+      },
+      head: [['ID', 'Nome', 'Embalagem Abreviada', 'Preço']],
+      body: filteredData.map(product => [
+        product.id,
+        product.nome,
+        product.emb_abreviada,
+        product.preco
+      ]),
+      startY: 20
+    });
+    doc.save('products_data.pdf');
+  }
+}
+
+// const exportarCSV = () => {
+//     carregando.value = true;
+//     api.get('/catalogo/grid/exportar-csv', { responseType: 'blob' })
+//     .then((response) => {
+//         const url = window.URL.createObjectURL(new Blob([response.data]));
+//         const link = document.createElement('a');
+//         link.href = url;
+//         link.setAttribute('download', 'produtos.csv');
+//         document.body.appendChild(link);
+//         link.click();
+//         link.parentNode.removeChild(link);
+//     })
+//     .catch((error) => {
+//         console.log(error);
+//     })
+//     .finally(() => {
+//         carregando.value = false;
+//     });
+// }
 
 /**
  * Hooks
